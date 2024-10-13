efc7410efba94d9bf184e5205f3ddb3e51f9f81a
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-10-13 17:31:19 +0100

    Homepage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import Homepage from './components/Homepage'
 
 
 function App() {
 
   return (
     <>
-      <h1 className=''>Hello</h1>
+      <div className='flex flex-col max-w[1000px] mx-auto w-full'>
+        <section className='min-h-screen flex flex-col'>
+          <header className='flex item-center justify-between gap-4 p-4'>
+            <h1>Free <span className='text-blue-400'>Scribe</span></h1>
+            <button className='flex item-center gap-2'><p>New</p> <i className="fa-solid fa-plus"></i></button>
+          </header>
+          <Homepage/>
+        </section>
+      </div>
     </>
   )
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <main className='flex-1 p-4 flex-col justify-center'>
+            
+    </main>
+  )
+}
+
+export default Homepage
